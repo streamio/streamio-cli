f0ce9173ee6a0869696ad647c5038b7196b45eab
David Backeus <duztdruid@gmail.com>
2012-12-04 15:39:26 +0100

    Ruby 1.8 compatible spec.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,30 +7,30 @@ describe Streamio::CLI::App do
   describe "#export" do
     it "should look for videos and audios of target account and download them" do
       stub_request(:get, "https://streamio:passw0rd@streamio.com/api/v1/videos/count").
-        to_return(status: 200, body: json(count: 1))
+        to_return(:status => 200, :body => json(:count => 1))
 
       stub_request(:get, "https://streamio:passw0rd@streamio.com/api/v1/videos?limit=100&skip=0").
-        to_return(status: 200, body: webmock_fixture("videos.json"))
+        to_return(:status => 200, :body => webmock_fixture("videos.json"))
 
       stub_request(:head, "http://d253c4ja9jigvu.cloudfront.net/original_videos/4c50424cb35ea827c0000005_4ce53c17b35ea86d7300000g.mp4").
-        to_return(status: 200, headers: {'Content-Length' => 3795530})
+        to_return(:status => 200, :headers => {'Content-Length' => 3795530})
 
       stub_request(:get, "http://d253c4ja9jigvu.cloudfront.net/original_videos/4c50424cb35ea827c0000005_4ce53c17b35ea86d7300000g.mp4").
-        to_return(status: 200)
+        to_return(:status => 200)
 
       stub_request(:get, "https://streamio:passw0rd@streamio.com/api/v1/audios/count").
-        to_return(status: 200, body: json(count: 1))
+        to_return(:status => 200, :body => json(:count => 1))
 
       stub_request(:get, "https://streamio:passw0rd@streamio.com/api/v1/audios?limit=100&skip=0").
-        to_return(status: 200, body: webmock_fixture("audios.json"))
+        to_return(:status => 200, :body => webmock_fixture("audios.json"))
 
       stub_request(:head, "http://drtpe3yp3e35w.cloudfront.net/original_audios/4de3a1d0541290fd3000000e_4f96806c505ca25d71000001.mp3").
-        to_return(status: 200, headers: {'Content-Length' => 3795530})
+        to_return(:status => 200, :headers => {'Content-Length' => 3795530})
 
       stub_request(:get, "http://drtpe3yp3e35w.cloudfront.net/original_audios/4de3a1d0541290fd3000000e_4f96806c505ca25d71000001.mp3").
-        to_return(status: 200)
+        to_return(:status => 200)
 
-      subject.options = { username: "streamio", password: "passw0rd" }
+      subject.options = { :username => "streamio", :password => "passw0rd" }
       subject.export
 
       WebMock.should have_requested(:get, "http://d253c4ja9jigvu.cloudfront.net/original_videos/4c50424cb35ea827c0000005_4ce53c17b35ea86d7300000g.mp4")
